bac8483a66440fdbc428c1f2c8a96213e59eb624
Leandro Marin <marinlopezleandro@gmail.com>
2023-02-01 21:57:35 -0300

    day 07 - feat: invalid gifts or repeated wont be added

---
+++ day-07/script.js
@@ -0,0 +1,94 @@
+//SELECT ITEMS
+const giftList = document.querySelector('#list-of-gifts');
+const form = document.querySelector(".gift-list-form");
+const gift = document.querySelector('#input-gift');
+const deleteAllBtn = document.querySelector('#btn-delall');
+const initialMessage = document.querySelector('#initial-message');
+
+//EVENT LISTENERS
+//ADD NEW GIFT
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const giftExist = giftExists(gift.value);
+  
+  if (gift.value === "" || giftExist) {
+    gift.classList.add('shake');
+    setTimeout(() => {gift.classList.remove('shake')}, 500);
+  };
+  
+  if (gift.value !== "" && giftExist === false) {
+    addItem(gift.value);
+    storeItem(gift.value);
+    gift.value = "";
+  };
+  
+  checkInitialMessage();
+});
+
+//REMOVE GIFT WHEN I CLICK IN IT
+giftList.addEventListener('click', (event) => {
+  event.target.remove();
+  
+  listOfGifts = deleteItemFromStorage(event.target.textContent);
+
+  checkInitialMessage();
+});
+
+//DELETE ALL GIFT's
+deleteAllBtn.addEventListener('click', (event) => {
+  deleteAllItems(giftList);
+
+  listOfGifts = [];
+  checkInitialMessage()
+});
+
+//MOCK DATA
+let listOfGifts = [];
+
+//FUNCTIONS
+function initializeList() {
+  /* if (listOfGifts.length === 0) initialMessage. */
+  listOfGifts.forEach(item => addItem(item))
+};
+
+function addItem(item) {
+  const newElement = document.createElement('li');
+  newElement.textContent = item;
+  giftList.appendChild(newElement)
+};
+
+function storeItem(item) {
+  //This mutate the array
+  /* listOfGifts.push(item) */
+  
+  //This do not
+  listOfGifts = [...listOfGifts, item]
+};
+
+function deleteItemFromStorage(item) {
+  const indexItem = listOfGifts.indexOf(item);
+  return listOfGifts.filter(element => element !== item)
+};
+
+function deleteAllItems(item) {
+  let child = item.lastElementChild;
+  while (child) {
+    item.removeChild(child);
+    child = item.lastElementChild
+  }
+};
+
+function checkInitialMessage() {
+  if (listOfGifts.length !== 0) {
+    initialMessage.classList.add('hidden')
+  } else {
+    initialMessage.classList.remove('hidden')
+  }
+};
+
+function giftExists(gift) {
+  return listOfGifts.some(element => element.toLowerCase() === gift.toLowerCase())
+};
+
+initializeList();
+checkInitialMessage();
